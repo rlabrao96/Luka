77c49c445d065d4c29c25a09cca64fc62efabec8
rlabrao96 <rafaellabra96@gmail.com>
2026-03-18 02:09:16 -0400

    feat: add ImportStatusBanner to dashboard layout

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -2,6 +2,7 @@ import { Sidebar } from "./components/Sidebar";
 import { BottomNav } from "./components/BottomNav";
 import { StoreInitializer } from "./components/StoreInitializer";
 import { InactivityGuard } from "./components/InactivityGuard";
+import { ImportStatusBanner } from "./components/ImportStatusBanner";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,6 +13,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <Sidebar />
       {/* Main scrolling area */}
       <main className="flex-1 overflow-y-auto pb-20 lg:pb-0">
+        <ImportStatusBanner />
         <div className="max-w-5xl mx-auto px-4 sm:px-6 py-6 sm:py-8">
           {children}
         </div>
